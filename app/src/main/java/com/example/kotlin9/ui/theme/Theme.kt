0ebf863f92328587829dbdaa332a7e50a6546a22
Lisa <elbondarenko04@gmail.com>
2024-11-06 20:45:24 +0300

    .

--- app/src/main/java/com/example/kotlin9/ui/theme/Theme.kt
+++ app/src/main/java/com/example/kotlin9/ui/theme/Theme.kt
@@ -12,15 +12,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = LightGray,
-    secondary = DarkGray,
-    tertiary = Black
-)
-
-private val LightColorScheme = lightColorScheme(
     primary = Green90,
     secondary = Beige100,
     tertiary = Gray50
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = LightGray,
+    secondary = DarkGray,
+    tertiary = Black
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
